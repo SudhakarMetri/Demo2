6c6b4ea25c6de9bd7df0a39f15f2c67effde40d9
sudhakar metri <sudhakar metri@DESKTOP-ICNMCCS>
2022-03-14 09:53:26 +0530

    Pushed on monday

--- src/test/java/PracticeDemo/DemoPush.java
+++ src/test/java/PracticeDemo/DemoPush.java
@@ -12,5 +12,16 @@ public void demo()
 	System.out.println("User 3 ---> First Commit");
 	System.out.println("User 4 ---> First Commit");
 
+	System.out.println("user1 first commit");
+	System.out.println("user2 2nd commit");
+
+	System.out.println("User 3 ---> First Commit");
+	System.out.println("User 4 ---> First Commit");
+	System.out.println("user1 first commit");
+	System.out.println("user2 2nd commit");
+
+	System.out.println("User 3 ---> First Commit");
+	System.out.println("User 4 ---> First Commit");
+
 	}
 }

2796c512f4f0d7131349e56631ff91c981ba49ab
sudhakar metri <sudhakar metri@DESKTOP-ICNMCCS>
2022-03-14 17:06:15 +0530

    pusshhhhhh

---
+++ src/test/java/PracticeDemo/demo3test.java
@@ -0,0 +1,16 @@
+package PracticeDemo;
+
+import org.testng.annotations.Test;
+
+public class demo3test {
+
+	@Test
+	public void rest()
+	{
+		String browser=System.getProperty("Browser");
+		System.out.println(browser);
+		String url=System.getProperty("Browser");
+		System.out.println();
+				
+	}
+}

9475c68b3505bc5ea4419f0db2f1632769a15b01
sudhakar metri <sudhakar metri@DESKTOP-ICNMCCS>
2022-03-14 15:55:23 +0530

    push

---
+++ src/test/java/PracticeDemo/Demo2test.java
@@ -0,0 +1,24 @@
+package PracticeDemo;
+
+import org.testng.annotations.Test;
+
+public class Demo2test
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("User 1");
+		
+		System.out.println(" this is Soul");
+		
+		
+		System.out.println("Trying to contact");
+		
+		System.out.println("connection lost");
+System.out.println("this is dark soul now");
+		
+		System.out.println("this is what u get ");
+		
+	}
+
+}
